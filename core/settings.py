bc2450f86840186817a360623e7c31fbb703e645
michael <michaelutoh21@gmail.com>
2022-11-15 21:33:02 +0100

    Fix requirements file

--- core/settings.py
+++ core/settings.py
@@ -134,7 +134,12 @@ WEB_TOKEN_EXPIRY = 7200
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
